cd31f01c6dfbfc80cff637314f5e2e68c28fdb22
Ramavtar Nagar <ramavtarnagar13@gmail.com>
2024-08-11 20:14:19 +0530

    Button Component Completed

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,14 +1,29 @@
 import ButtonSvg from '../assets/svg/ButtonSvg'
 
 const Button = ({ className, href, onClick, children, px, white }) => {
+  
+  const classes = `button relative inline-flex items-center justify-center h11 transition-colors hover:text-color-1 
+                  ${px || 'px-7'} ${white ? 'text-n-8' : 'text-n-8'} ${className || ''}`;
+
+  const spanClasses = 'relative z-10'
+
   const renderButton = () => {
-    <button>
-        <span>{children}</span>
+    <button className= {classes}>
+        <span className= {spanClasses}>{children}</span>
         {ButtonSvg(white)}
     </button>
   };
 
-  return renderButton();
+  const renderlink = () => (
+    <a href={href}>
+      <span className={spanClasses}>{children}</span>
+      {ButtonSvg(white)}
+    </a>
+  )
+
+  return href 
+    ? renderlink()
+    : renderButton();
 };
 
 export default Button
