6e6680121720665f2ea378296437b3a680bae35a
Ramavtar Nagar <ramavtarnagar13@gmail.com>
2024-08-12 12:03:54 +0530

    Header Added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,14 @@
-import {brainwave} from "../assets";
+import { brainwave } from "../assets";
+import { navigation } from "../constants";
+import { useLocation } from 'react-router-dom'
+import Button from './Button'
+
 const Header = () => {
+
+  const pathname = useLocation();
+
   return (
-    <div className="fixed top-0 z-50 bg-n-8/90 backdrop-blur-sm border-b border-n-6 
+    <div className="fixed top-0 left-0 w-full z-50 bg-n-8/90 backdrop-blur-sm border-b border-n-6 
     lg:bg-n-8/90 lg:backdrop-blur-sm">
         <div className="flex items-center px-5 lg:px-7.5 xl:px-10 max-lg:py-4">
         
@@ -10,9 +17,23 @@ const Header = () => {
         </a>
 
         <nav className="hidden fixed top-[5rem] left-0 right-0 bottom-0 bg-n-8 lg:static lg:flex lg:mx-auto lg:bg-transparent">
-            <div className="relative z-2 flex flex-col justify-center items-center m-auto lg:flex-row"></div>
+            <div className="relative z-2 flex flex-col justify-center items-center m-auto lg:flex-row">
+              {navigation.map((items) => (
+                <a key={items.id} href={items.url}
+                className={`block relative font-code text-2xl uppercase text-n-1 transition-colors hover:text-color-1 ${items.onlyMobile ? "lg:hidden" : ""} 
+                px-6 py-6 md:py-8 lg:-mr-0.25 lg:text-xs lg:font-semibold ${items.url === pathname.hash ? 'z-2 lg:text-n-1' : 'lg:text-n-1/50'}
+                lg:leading-5 lg:hover:text-n-1 xl:px-12`}>
+                  {items.title}
+              </a>
+              ))}
+            </div>
         </nav>
-
+        <a href="#signup" className="button hidden mr-8 transition-colors text-n-1/50 hover:text-n-1 lg:block">
+              New Account
+        </a>
+        <Button className="hidden lg:flex" href="#login">
+          Sign In
+        </Button>
         </div>
     </div>
   )
