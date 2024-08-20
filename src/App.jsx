7d3e8c4bdccf1b4ebb701b58cc21f7a75bef3796
Ramavtar Nagar <ramavtarnagar13@gmail.com>
2024-08-20 14:42:20 +0530

    Section.jsx Code Added

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import ButtonGradient from './assets/svg/ButtonGradient'
+import Benefits from './components/Benefits'
 import Button from './components/Button'
 import Header from './components/Header'
 
