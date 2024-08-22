f499b6d5f7a9191e1172e8f7e8c9d89eefeb8408
Ramavtar Nagar <ramavtarnagar13@gmail.com>
2024-08-22 22:12:39 +0530

    Hero section started

---
+++ src/components/Hero.jsx
@@ -0,0 +1,28 @@
+import { curve, heroBackground, robot } from "../assets";
+import Button from "./Button";
+import Section from "./Section";
+import { BackgroundCircles, BottomLine, Gradient } from "./design/Hero";
+import { heroIcons } from "../constants";
+import { ScrollParallax } from "react-just-parallax";
+import { useRef } from "react";
+import Generating from "./Generating";
+import Notification from "./Notification";
+import CompanyLogos from "./CompanyLogos";
+
+const Hero = () => {
+  const parallaxRef = useRef(null);
+
+  return (
+    <Section
+      className="pt-[12rem] -mt-[5.25rem]"
+      crosses
+      crossesOffset="lg:translate-y-[5.25rem]"
+      customPaddings
+      id="hero"
+    >
+
+    </Section>
+  );
+};
+    
+export default Hero;
