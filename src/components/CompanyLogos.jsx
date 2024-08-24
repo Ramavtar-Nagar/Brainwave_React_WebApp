0e2536dfe09803bd39fee86ed3b0b6f0f9c10521
Ramavtar Nagar <ramavtarnagar13@gmail.com>
2024-08-24 09:28:02 +0530

    CompanyLogos Utility Addded

---
+++ src/components/CompanyLogos.jsx
@@ -0,0 +1,28 @@
+import { companyLogos } from "../constants";
+
+const CompanyLogos = ( className) => {
+  return (
+    <div className={className}>
+        <h5 className="tagline mb-6 text-center text-n-1/50">
+            Helping people create beautiful content at
+        </h5>
+        <ul className="flex">
+            {companyLogos.map((logo, index) => (
+                <li
+                key={index}
+                className="flex items-center justify-center flex-1 h-[8.5rem]"
+                >
+                    <img 
+                    src={logo}
+                    height={134}
+                    width={134}
+                    alt={logo}
+                    />
+                </li>
+            ))}
+        </ul>
+    </div>
+  );
+};
+
+export default CompanyLogos;
